16d9bdb26cf2a967b0b6acd57f6d16df5e9b55b7
andreafrancioni <andreafrancioni.work@gmail.com>
2024-02-05 17:11:31 +0100

    debug string name create tags table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
+        $this->call(TagSeeder::class);
         $this->call(EventSeeder::class);
     }
 }
