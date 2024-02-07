7166d3a0dd0db1699fba160e9b00e32639cab741
andreafrancioni <andreafrancioni.work@gmail.com>
2024-02-07 19:13:44 +0100

    Richiama api recuperando model event test postman

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,10 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get("/prova", function () {
-    $dati = [
-        "nome" => "Andrea",
-        "cognome" => "Francioni"
-    ];
+    // $dati = [
+    //     "nome" => "Andrea",
+    //     "cognome" => "Francioni"
+    // ];
+
+    $dati = Event::all();
 
     return response()->json($dati);
 });
